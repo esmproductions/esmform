e961c3c85e1b67a9cab9cd87b2fadc7c13e7c6fd
Joseph VanderStel <jvan@Wow5kiMac.local>
2017-10-19 20:44:47 -0400

    changed cdn to course id, added parent label

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,15 +2,17 @@ $(document).ready(function(){
   $('#main-form').parsley()
   $('.add-cdn').click(function(){
     $('#main-form').parsley().destroy()
-    var num_cdns = document.getElementsByClassName('cdn').length
-    console.log(num_cdns)
-    var $div = $('<div></div>').addClass('form-group cdn')
+    var num_cids = document.getElementsByClassName('cid').length
+    console.log(num_cids)
+    var $div = $('<div></div>').addClass('form-group cid')
     var $inputGroup = $('<div></div>').addClass('input-group')
-    var $label = $('<label></label>').text(`CDN #${num_cdns+1}`).addClass('cdn-num')
+    var $label = $('<label></label>').text(`Course ID #${num_cids+1}`).addClass('cid-num')
     var $input = $('<input></input>').attr({
+      'id': num_cids,
+      'name': num_cids,
       'class': 'form-control',
       'type': 'text',
-      'data-parsley-cdn': '',
+      'data-parsley-cid': '',
       'required': '',
       'data-parsley-trigger': 'change'
     })
@@ -21,19 +23,19 @@ $(document).ready(function(){
 
     $inputGroup.append([$input,$span])
     $div.append([$label,$inputGroup])
-    $('.cdns').append($div)
+    $('.cids').append($div)
     $('#main-form').parsley()
   })
 })
 
 $(document).on('click','.remove-cdn', function(){
   $(this).parent('div').parent('div').remove()
-  var num_cdns = $('.cdn').length
+  var num_cids = $('.cid').length
   var count = 1
-  $('.cdn').each(function(e){
-    $(this).children('label').text(`CDN #${count}`)
+  $('.cid').each(function(e){
+    $(this).children('label').text(`Course ID #${count}`)
     count = count + 1
   })
-  console.log(num_cdns)
+  console.log(num_cids)
 
 })
