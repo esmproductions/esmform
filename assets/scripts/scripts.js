e941b6426d9922fb7d3e91ab3c340a1d1e43b7d8
Joseph VanderStel <jvan@Wow5kiMac.local>
2017-11-13 14:02:00 -0500

    working on email heading

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,26 +2,26 @@ $(document).ready(function(){
   $('#main-form').parsley()
   $('.add-cid').click(function(){
     $('#main-form').parsley().destroy()
-    var num_cids = document.getElementsByClassName('cid').length
+    var this_cid = document.getElementsByClassName('cid').length + 1
     var $div = $('<div></div>').addClass('form-group cid')
     var $inputGroup = $('<div></div>').addClass('input-group')
-    var $label = $('<label></label>').text(`Course ID #${num_cids+1}`).addClass('cid-num')
+    var $label = $('<label></label>').text(`Course ID #${this_cid}`).addClass('cid-num')
     var $input = $('<input></input>').attr({
-      'id': `${ordinal(num_cids+1)} ID`,
-      'name': `${ordinal(num_cids+1)} ID`,
+      'id': `${ordinal(this_cid)} ID`,
+      'name': `${ordinal(this_cid)} ID`,
       'class': 'form-control',
       'type': 'text',
       'data-parsley-cid': '',
       'required': '',
       'data-parsley-trigger': 'change',
-      'data-parsley-errors-container': `.error${num_cids}`
+      'data-parsley-errors-container': `.error${this_cid}`
     })
     var $span = $('<span></span>').attr({
       'type': 'button',
       'class': 'btn btn-danger input-group-addon remove-cid'
     }).text('remove')
 
-    var $field = $('<div></div>').addClass(`error${num_cids}`)
+    var $field = $('<div></div>').addClass(`error${this_cid}`)
 
     $inputGroup.append([$input,$span])
     $div.append([$label,$inputGroup,$field])
@@ -37,13 +37,11 @@ $(document).ready(function(){
 
 $(document).on('click','.remove-cid', function(){
   $(this).parent('div').parent('div').remove()
-  var num_cids = $('.cid').length
   var count = 1
   $('.cid').each(function(e){
     $(this).children('label').text(`Course ID #${count}`)
     count = count + 1
   })
-
 })
 
 function ordinal(num) {
